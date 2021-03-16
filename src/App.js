56949bb2cd546cdc322ae328719fa46aab496450
Bisss <bisss@Bissss-MacBook-Pro.local>
2021-03-16 20:14:53 +0200

    Third party components added

--- src/App.js
+++ src/App.js
@@ -1,51 +1,89 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
-import "./App.css";
-import Todotablee from "./components/Todolist";
+import React, { useState, useRef } from "react";
+import { AgGridReact } from "ag-grid-react";
 
-function App() {
-  const [todo, setTodo] = React.useState({ desc: "", date: "" });
-  const [todos, setTodos] = React.useState([]);
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-material.css";
+
+function Todolist() {
+  const [todo, setTodo] = useState({ description: "", date: "", priority: "" });
+  const [todos, setTodos] = useState([]);
+
+  const gridRef = useRef();
 
   const inputChanged = (event) => {
     setTodo({ ...todo, [event.target.name]: event.target.value });
   };
 
   const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, todo]); // all values of todos array and objects of todo
+    setTodos([...todos, todo]);
   };
 
-  const deleteRow = (index) => {
-    setTodos(todos.filter((todo, i) => i !== index));
+  const deleteTodo = () => {
+    setTodos(
+      todos.filter(
+        (todo, index) =>
+          index !== gridRef.current.getSelectedNodes()[0].childIndex
+      )
+    );
   };
+
+  const columns = [
+    {
+      headername: "Description",
+      field: "description",
+      sortable: "true",
+      filter: "true",
+    },
+    { headername: "Date", field: "date", sortable: "true", filter: "true" },
+    {
+      headername: "Priority",
+      field: "priority",
+      sortable: "true",
+      filter: "true",
+      cellStyle: (params) =>
+        params.value === "High" ? { color: "red" } : { color: "black" },
+    },
+  ];
 
   return (
-    <div className="App">
-      <h1 className="App-header">Add todo:</h1>
-      <form onSubmit={addTodo} class="body">
-        <fieldset>
-          <legend>TodoList:</legend>
-          Description:
-          <input
-            type="text"
-            name="desc"
-            value={todo.desc}
-            onChange={inputChanged}
-          />
-          Date:
-          <input
-            type="date"
-            name="date"
-            value={todo.date}
-            onChange={inputChanged}
-          />
-          <input type="submit" value="Add" />
-        </fieldset>
-      </form>
-      <Todotablee alltodos={todos} delete={deleteRow} />
+    <div>
+      <input
+        type="text"
+        onChange={inputChanged}
+        placeholder="Description"
+        name="description"
+        value={todo.description}
+      />
+      <input
+        type="date"
+        onChange={inputChanged}
+        placeholder="Date"
+        name="date"
+        value={todo.date}
+      />
+      <input
+        type="text"
+        onChange={inputChanged}
+        placeholder="Priority"
+        name="priority"
+        value={todo.priority}
+      />
+      <button onClick={addTodo}>Add</button>
+      <button onClick={deleteTodo}>Delete</button>
+      <div
+        className="ag-theme-material"
+        style={{ width: "50%", height: "700px", margin: "auto" }}
+      >
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={(params) => (gridRef.current = params.api)}
+          rowSelection="single"
+          columnDefs={columns}
+          rowData={todos}
+        />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Todolist;
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -9,12 +9,14 @@ export default function Todolist(props) {
           <tr>
             <th>Description</th>
             <th>Date</th>
+            <th>Priority</th>
             <th>Delete</th>
           </tr>
           {props.alltodos.map((todo, index) => (
             <tr key={index}>
               <td>{todo.desc}</td>
               <td>{todo.date}</td>
+              <td>{todo.priority}</td>
               <td>
                 <button onClick={() => props.delete(index)}>Delete</button>
               </td>
